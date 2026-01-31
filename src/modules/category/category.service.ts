79b59903bc998b34752c5c3796b36a5898eba265
sabbir <sabbir00921@gmail.com>
2026-01-31 06:50:53 +0600

    implement update and get all category

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,6 +1,7 @@
-import { uploadCloudinary } from "../../helpers/cloudinary";
+import { deleteCloudinary, uploadCloudinary } from "../../helpers/cloudinary";
 import CustomError from "../../helpers/CustomError";
-import { ICreateCategory } from "./category.interface";
+import { paginationHelper } from "../../utils/pagination";
+import { ICreateCategory, IUpdateCategory } from "./category.interface";
 import { CategoryModel } from "./category.models";
 
 
@@ -21,6 +22,90 @@ const createCategory = async (data: ICreateCategory, images: any) => {
     return category;
 };
 
+//get all categories
+const getAllCategories = async ({
+    page = 1,
+    limit = 10,
+    search,
+    status = "all",
+}: any) => {
+
+    // Pagination (always defined)
+    const pagination = paginationHelper(page, limit);
+
+    // Base filter
+    const filter: any = {
+        isDeleted: false,
+    };
+
+    // Status filter
+    if (status !== "all") {
+        if (status !== "active" && status !== "inactive") throw new CustomError(400, "Invalid status, available status: active, inactive");
+        filter.status = status;
+    }
+
+    // Search filter
+    if (search) {
+        filter.name = { $regex: search, $options: "i" };
+    }
+
+    const categories = await CategoryModel.find(filter)
+        .skip(pagination.skip)
+        .limit(pagination.limit)
+        .populate("subCategoriesId");
+
+    if (!categories.length) {
+        throw new CustomError(404, "Categories not found");
+    }
+
+    // Correct count
+    const totalItems = await CategoryModel.countDocuments(filter);
+
+    return {
+        categories,
+        pagination: {
+            page: pagination.page,
+            limit: pagination.limit,
+            totalItems,
+            totalPages: Math.ceil(totalItems / pagination.limit),
+        },
+    };
+};
+
+
+//get single category
+const getSingleCategory = async (categoryId: string) => {
+    const category = await CategoryModel.findById({ _id: categoryId, isDeleted: false, status: "active" }).populate("subCategoriesId");
+    if (!category) throw new CustomError(400, "Category not found");
+    return category;
+}
+
+
+//update category
+const updateCategory = async (categoryId: string, data: IUpdateCategory, images: any) => {
+    const category = await CategoryModel.findOneAndUpdate({ _id: categoryId }, data, { new: true });
+    if (!category) throw new CustomError(400, "Category not found");
+
+    //if images
+    if (images.path) {
+        //delete previous image
+        if (category?.image?.public_id) {
+            await deleteCloudinary(category?.image?.public_id);
+        }
+        //upload new image && save
+        const uploadedImage = await uploadCloudinary(images?.path);
+        if (uploadedImage) {
+            category.image = uploadedImage;
+            await category.save();
+        }
+    }
+    return category;
+}
+
+
 export const categoryService = {
     createCategory,
+    getAllCategories,
+    getSingleCategory,
+    updateCategory
 };
--- src/modules/category/category.validation.ts
+++ src/modules/category/category.validation.ts
@@ -5,11 +5,28 @@ export const createCategorySchema = z.object({
         .string()
         .min(3, "Title must be at least 3 characters")
         .max(50, "Title must be at most 50 characters")
-        .transform((val) => val.trim()),
+        .optional()
+        .transform((val) => (val ? val.trim() : undefined)),
 
     description: z
         .string()
         .max(500, "Description must be at most 500 characters")
         .optional()
-        .transform((val) => (val ? val.trim() : undefined))
+        .transform((val) => (val ? val.trim() : undefined)),
+    status: z.string().optional()
+});
+
+export const updateCategorySchema = z.object({
+    title: z
+        .string()
+        .min(3, "Title must be at least 3 characters")
+        .max(50, "Title must be at most 50 characters")
+        .optional()
+        .transform((val) => (val ? val.trim() : undefined)),
+    description: z
+        .string()
+        .max(500, "Description must be at most 500 characters")
+        .optional()
+        .transform((val) => (val ? val.trim() : undefined)),
+    status: z.string().optional()
 });
