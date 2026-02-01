63b7e8052f298a8aedd918eea4e7d00d2f6c094b
sabbir <sabbir00921@gmail.com>
2026-02-02 03:32:51 +0600

    Create topics api implemented

--- src/routes/index.api.ts
+++ src/routes/index.api.ts
@@ -1,12 +1,14 @@
 import express from "express";
 const router = express.Router();
 
+import { userRoute } from "../modules/usersAuth/user.route";
 import subject from "../modules/subject/subject.route";
 import label from "../modules/label/label.routes";
-import { userRoute } from "../modules/usersAuth/user.route";
+import topic from "../modules/topic/topic.routes";
 
 router.use("/user", userRoute);
 router.use("/subject", subject);
 router.use("/label", label);
+router.use("/topic", topic);
 
 export default router;
