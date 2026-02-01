202ce59644ef3988c26cd40c3d43bcb6183e6ac9
sabbir <sabbir00921@gmail.com>
2026-02-02 05:04:08 +0600

    Protect all routes (subject, label, topic)

--- src/modules/label/label.routes.ts
+++ src/modules/label/label.routes.ts
@@ -3,13 +3,14 @@ import { createLabel, deleteLabel, getAllLabel, getSingleLabel, updateLabel } fr
 import { validateRequest } from "../../middleware/validateRequest.middleware";
 import { createLabelSchema, updateLabelSchema } from "./label.validation";
 import { uploadSingle } from "../../middleware/multer.midleware";
+import { allowRole, authGuard } from "../../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.post("/create-label", uploadSingle("image"), validateRequest(createLabelSchema), createLabel);
-router.get("/get-label/:labelId", getSingleLabel);
+router.post("/create-label", authGuard, allowRole("admin"), uploadSingle("image"), validateRequest(createLabelSchema), createLabel);
+router.get("/get-label/:labelId", authGuard, allowRole("admin", "user"), getSingleLabel);
 router.get("/get-all-label", getAllLabel);
-router.patch("/update-label/:labelId", uploadSingle("image"), validateRequest(updateLabelSchema),  updateLabel);
-router.delete("/delete-label/:labelId",  deleteLabel);
+router.patch("/update-label/:labelId",authGuard, allowRole("admin"), uploadSingle("image"), validateRequest(updateLabelSchema), updateLabel);
+router.delete("/delete-label/:labelId",authGuard, allowRole("admin"), deleteLabel);
 
 export default router;
--- src/modules/topic/topic.routes.ts
+++ src/modules/topic/topic.routes.ts
@@ -3,13 +3,14 @@ import { createTopic, getAllTopic, getSingleTopic, updateTopic, deleteTopic } fr
 import { validateRequest } from "../../middleware/validateRequest.middleware";
 import { createTopicSchema, updateTopicSchema } from "./topic.validation";
 import { uploadSingle } from "../../middleware/multer.midleware";
+import { allowRole, authGuard } from "../../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.post("/create-topic", uploadSingle("image"), validateRequest(createTopicSchema), createTopic);
+router.post("/create-topic", authGuard, allowRole("admin"), uploadSingle("image"), validateRequest(createTopicSchema), createTopic);
 router.get("/get-topic/:topicId", getSingleTopic);
-router.get("/get-all-topic", getAllTopic);
-router.patch("/update-topic/:topicId", uploadSingle("image"), validateRequest(updateTopicSchema), updateTopic);
-router.delete("/delete-topic/:topicId", deleteTopic);
+router.get("/get-all-topic", authGuard, allowRole("admin", "user"), getAllTopic);
+router.patch("/update-topic/:topicId", authGuard, allowRole("admin"), uploadSingle("image"), validateRequest(updateTopicSchema), updateTopic);
+router.delete("/delete-topic/:topicId", authGuard, allowRole("admin"), deleteTopic);
 
 export default router;
