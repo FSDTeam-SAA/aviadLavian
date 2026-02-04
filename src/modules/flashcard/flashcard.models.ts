2319369d686604a52d49201ef9859028fe611be0
sabbir <sabbir00921@gmail.com>
2026-02-05 05:44:57 +0600

    Flashcard CURD operation complete

--- src/modules/flashcard/flashcard.models.ts
+++ src/modules/flashcard/flashcard.models.ts
@@ -1,51 +1,45 @@
 import mongoose, { Schema } from "mongoose";
-    import slugify from "slugify";
-    import CustomError from "../../helpers/CustomError";
+import slugify from "slugify";
+import CustomError from "../../helpers/CustomError";
 import { IFlashcard } from "./flashcard.interface";
 
 //TODO: customize as needed
 
 const flashcardSchema = new Schema<IFlashcard>({
-  title: { type: String, required: true },
-  description: { type: String },
-  status: { type: String, default: "active" },
-  isDeleted: { type: Boolean, default: false },
-  slug: { type: String },
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
+  image: {
+    public_id: {
+      type: String,
+      required: false,
+    },
+    secure_url: {
+      type: String,
+      required: false,
+    },
+  },
+  topicId: [{
+    type: String,
+    required: true,
+  }],
+  difficulty: {
+    type: String,
+    required: true,
+    default: "medium",
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: true,
+  }
 }, { timestamps: true });
 
-// Generate slug before save
-flashcardSchema.pre("save", async function (next) {
-  if (!this.isModified("title")) return;
 
-  const category = await FlashcardModel.findOne({ title: this.title });
-  if (category) {
-    throw new CustomError(400, "Flashcard already exist");
-  }
-
-  this.slug = slugify(this.title, {
-    lower: true,
-    strict: true,
-    trim: true,
-  });
-});
-
-// Generate slug on update
-flashcardSchema.pre("findOneAndUpdate", async function () {
-  const update = this.getUpdate() as any;
-
-  const category = await FlashcardModel.findOne({ title: update.title });
-  if (category) {
-    throw new CustomError(400, "Flashcard already exist");
-  }
-
-  if (update?.title) {
-    update.slug = slugify(update.title, {
-      lower: true,
-      strict: true,
-      trim: true,
-    });
-  }
-
-});
 
 export const FlashcardModel = mongoose.model<IFlashcard>("Flashcard", flashcardSchema);
--- src/routes/index.api.ts
+++ src/routes/index.api.ts
@@ -2,9 +2,11 @@ import express from "express";
 const router = express.Router();
 
 import { userRoute } from "../modules/usersAuth/user.route";
+import flashcardRoute from "../modules/flashcard/flashcard.routes";
 
 
 router.use("/user", userRoute);
+router.use("/flashcard", flashcardRoute);
 
 
 export default router;
