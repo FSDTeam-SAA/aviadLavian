4db0680564d2a279ffa25ee05285ae67f90b1770
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2026-02-07 00:17:03 +0600

    add question

---
+++ src/modules/Question/question.controller.ts
@@ -0,0 +1,55 @@
+import { asyncHandler } from "../../utils/asyncHandler";
+import { questionService } from "./question.service";
+import ApiResponse from "../../utils/apiResponse";
+
+export const createQuestion = asyncHandler(async (req, res) => {
+  const question = await questionService.createQuestion({
+    ...req.body,
+    createdBy: (req as any).user._id,
+  });
+
+  ApiResponse.sendSuccess(res, 201, "Question created", question);
+});
+
+export const getAllQuestions = asyncHandler(async (_req, res) => {
+  const questions = await questionService.getAllQuestions();
+  ApiResponse.sendSuccess(res, 200, "Question list", questions);
+});
+
+export const getQuestionById = asyncHandler(async (req, res) => {
+  const question = await questionService.getQuestionById(
+    req.params.id as string,
+  );
+  ApiResponse.sendSuccess(res, 200, "Question details", question);
+});
+
+export const updateQuestion = asyncHandler(async (req, res) => {
+  const question = await questionService.updateQuestion(
+    req.params.id as string,
+    req.body,
+  );
+  ApiResponse.sendSuccess(res, 200, "Question updated", question);
+});
+
+export const deleteQuestion = asyncHandler(async (req, res) => {
+  const question = await questionService.deleteQuestion(
+    req.params.id as string,
+  );
+  ApiResponse.sendSuccess(res, 200, "Question deleted", question);
+});
+
+export const hideQuestion = asyncHandler(async (req, res) => {
+  const question = await questionService.toggleHideQuestion(
+    req.params.id as string,
+    true,
+  );
+  ApiResponse.sendSuccess(res, 200, "Question hidden", question);
+});
+
+export const unhideQuestion = asyncHandler(async (req, res) => {
+  const question = await questionService.toggleHideQuestion(
+    req.params.id as string,
+    false,
+  );
+  ApiResponse.sendSuccess(res, 200, "Question visible", question);
+});
---
+++ src/modules/Question/question.service.ts
@@ -0,0 +1,59 @@
+import { QuestionModel } from "./question.model";
+import CustomError from "../../helpers/CustomError";
+
+const createQuestion = async (payload: any) => {
+  return await QuestionModel.create(payload);
+};
+
+const getAllQuestions = async () => {
+  return await QuestionModel.find({ isDeleted: false }).populate(
+    "subTopicId",
+    "name",
+  );
+};
+
+const getQuestionById = async (id: string) => {
+  const question = await QuestionModel.findOne({
+    _id: id,
+    isDeleted: false,
+  });
+  if (!question) throw new CustomError(404, "Question not found");
+  return question;
+};
+
+const updateQuestion = async (id: string, payload: any) => {
+  const question = await QuestionModel.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  if (!question) throw new CustomError(404, "Question not found");
+  return question;
+};
+
+const deleteQuestion = async (id: string) => {
+  const question = await QuestionModel.findByIdAndUpdate(
+    id,
+    { isDeleted: true },
+    { new: true },
+  );
+  if (!question) throw new CustomError(404, "Question not found");
+  return question;
+};
+
+const toggleHideQuestion = async (id: string, isHidden: boolean) => {
+  const question = await QuestionModel.findByIdAndUpdate(
+    id,
+    { isHidden },
+    { new: true },
+  );
+  if (!question) throw new CustomError(404, "Question not found");
+  return question;
+};
+
+export const questionService = {
+  createQuestion,
+  getAllQuestions,
+  getQuestionById,
+  updateQuestion,
+  deleteQuestion,
+  toggleHideQuestion,
+};
