7e5a7908b3f49dfe2d12bbd7139162119fc75411
sabbir <sabbir00921@gmail.com>
2026-03-09 23:08:29 +0600

    test

--- src/modules/flashcardprogress/flashcardprogress.routes.ts
+++ src/modules/flashcardprogress/flashcardprogress.routes.ts
@@ -6,6 +6,7 @@ import { authGuard } from "../../middleware/auth.middleware";
 
 const router = express.Router();
 
+//why authGuard? Because we want to make sure that only authenticated users can create flashcard progress and view their progress. This ensures that the data is secure and personalized for each user.
 router.post("/create-review", authGuard, validateRequest(createFlashcardProgressSchema), reviewFlashcard);
 router.get("/my-progress/:topicId", authGuard, getFlashcardProgress);
 
